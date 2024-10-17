2a839470d4115d76ec0ade05d813f979d78f8783
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-10-17 03:03:12 +0300

    fix bug api cats

--- api/catApi.js
+++
@@ -1,9 +0,0 @@
-export async function getCatImages(limit = 10) {
-    const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}`, {
-        headers: {
-            'x-api-key': process.env.CAT_API_KEY
-        }
-    });
-    const data = await response.json();
-    return data;
-}
---
+++ api/cats.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+    const API_KEY = process.env.CAT_API_KEY;
+
+    try {
+        // Виконуємо запит до API котів
+        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=${API_KEY}`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data from Cat API');
+    }
+};
