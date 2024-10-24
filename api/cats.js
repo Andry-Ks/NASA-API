33131669661a17aee1091b6b9f6f83ee7214f07f
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-10-24 17:42:14 +0300

    added fav

--- api/cats.js
+++ api/cats.js
@@ -1,12 +1,12 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// module.exports = async (req, res) => {
-//     const API_KEY = process.env.CAT_API_KEY;
+module.exports = async (req, res) => {
+    const API_KEY = process.env.CAT_API_KEY;
 
-//     try {
-//         const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=${API_KEY}`);
-//         res.status(200).json(response.data);
-//     } catch (error) {
-//         res.status(500).send('Error fetching data from Cat API');
-//     }
-// };
+    try {
+        const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=${API_KEY}`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data from Cat API');
+    }
+};
